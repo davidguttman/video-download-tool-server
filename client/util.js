ad6f9f12144857a83ca6e0ab4ecfdc1839740906
David Guttman <david@davidguttman.com>
2021-10-11 07:55:13 -0700

    refactor: pull downloader component out

---
+++ client/util.js
@@ -0,0 +1,16 @@
+module.exports = {
+  toTimeStr,
+  pad
+}
+
+function toTimeStr (secs) {
+  const hours = Math.floor(secs / 3600)
+  const minutes = Math.floor(secs / 60 - hours * 60)
+  const seconds = Math.floor(secs - minutes * 60 - hours * 3600)
+
+  return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`
+}
+
+function pad (n) {
+  return n < 10 ? '0' + n : n
+}
